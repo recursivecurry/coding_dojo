4a6f9ac8174e41f6271eefa0781319281738702c
Jongsoo Lee <jongsoo.lee@lge.com>
2014-03-04 17:34:30 +0900

    CODECHEF - Bytelandian gold coins
    http://www.codechef.com/problems/COINS

---
+++ codechef/coins.py
@@ -0,0 +1,24 @@
+final = {}
+def exchange(n):
+    if n == 0:
+        return 0
+    elif n in final:
+        return final[n]
+    more = exchange(n/4)+exchange(n/3)+exchange(n/2)
+    if n < more:
+        final[n] = more
+        return more
+    else:
+        final[n] = n
+        return n
+ 
+if __name__ == '__main__':
+    result = []
+    for i in xrange(10):
+        num = raw_input()
+        if 0==len(num):
+            break
+        result.append(exchange(int(num)))
+ 
+    for r in result:
+        print r 
