a0d5f1f95fdc6acaf091c9f9ddc1b9a3b91e28fe
Jongsoo Lee <jongsoo.lee@lge.com>
2014-03-04 17:23:25 +0900

    ALGOSPOT - Endians
    http://algospot.com/judge/problem/read/ENDIANS

---
+++ algospot/endians.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Control.Monad
+import Data.Word
+import Data.Bits
+
+main :: IO ()
+main = getLine >>= \n -> replicateM (read n) getLine >>= \acts -> mapM_ (print . convertEndian . read) acts
+
+convertEndian :: Word32 -> Word32
+convertEndian x = (shift ((.&.) x (shift 255 24)) (-24)) + (shift ((.&.) x (shift 255 16)) (-8)) + (shift ((.&.) x (shift 255 8)) (8)) + (shift ((.&.) x (shift 255 0)) (24))
