71812d1cd3fb01765e54f6810597df14a348cdb7
unknown <jongsoo.lee@JONGSOO-LITTLE.LGE.NET>
2014-03-14 17:28:12 +0900

    CODECHEF: WALK
    http://www.codechef.com/MARCH14/problems/WALK

---
+++ codechef/walk.py
@@ -0,0 +1,13 @@
+# CODECHEF - Walk
+# http://www.codechef.com/MARCH14/problems/WALK
+if __name__ == '__main__':
+    T = int(raw_input())
+    for count in xrange(T):
+        V = 0
+        N = int(raw_input())
+        W = [int(w) for w in raw_input().split()]
+        for n in xrange(N-1, -1, -1):
+            V+=1
+            if V < W[n]:
+                V = W[n]
+        print V
