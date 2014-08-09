3872c3b8cadff88c7c9528a4a02b94a80eb6acd0
Jongsoo Lee <leejongsoo@gmail.com>
2014-08-10 01:23:15 +0900

    HACKERRANK: Maximizing-XOR
    https://www.hackerrank.com/challenges/maximizing-xor

---
+++ hackerrank/Algorithms/01-Warmup/MaximizingXOR/MaximizingXOR.cpp
@@ -0,0 +1,50 @@
+// HACKERRANK: Maximizing-XOR
+// https://www.hackerrank.com/challenges/maximizing-xor
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+ * Complete the function below.
+ */
+int maxXor(int l, int r) {
+
+    int sum = 0;
+    int pos = 1;
+
+    while (l != r) {
+        l /= 2;
+        r /= 2;
+        sum += pos;
+        pos *= 2;
+    }
+    return sum;
+}
+
+int main() {
+    int res;
+    int _l;
+    cin >> _l;
+    
+    int _r;
+    cin >> _r;
+    
+    res = maxXor(_l, _r);
+    cout << res;
+    
+    return 0;
+}
