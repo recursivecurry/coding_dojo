aa9e4187ff55b8ab252893d56928df56bdf2e459
Jongsoo Lee <jongsoo.lee@lge.com>
2014-08-29 13:22:01 +0900

    HACKERRANK: Sherlock and Queries (little updated)
    https://www.hackerrank.com/challenges/sherlock-and-queries

--- hackerrank/Algorithms/01-Warmup/SherlockAndQueries/SherlockAndQueries.cpp
+++ hackerrank/Algorithms/01-Warmup/SherlockAndQueries/SherlockAndQueries.cpp
@@ -2,38 +2,39 @@
 // https://www.hackerrank.com/challenges/sherlock-and-queries
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <cstdio>
 
 using namespace std;
 
 int main (int argc, char *argv[]) {
-    size_t N, M, count;
+    long unsigned int N, M, count;
     long long val;
     vector<long long> A, B, C;
     
-    cin >> N >> M;
+    scanf("%lu %lu", &N, &M);
 
     count = N;
     while (count--) {
-        cin >> val;
+        scanf("%lld", &val);
         A.push_back(val);
     }
     
     count = M;
     while (count--) {
-        cin >> val;
+        scanf("%lld", &val);
         B.push_back(val);
     }
     
     count = M;
     while (count--) {
-        cin >> val;
+        scanf("%lld", &val);
         C.push_back(val);
     }
 
-    for (size_t i=0; i<M; i++) {
-        for (size_t j=0; j<N; j++) {
-            if ( (j+1) % B[i] == 0 )
-                A[j] = (A[j] * C[i]) % 1000000007;
+    for (long unsigned int i=0; i<M; i++) {
+        for (long unsigned int j=B[i]-1; j<N; j+=B[i]) {
+            A[j] = (A[j] * C[i]) % 1000000007;
         }
     }
 
