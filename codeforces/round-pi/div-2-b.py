a57a7ae94a09083695a40ebe796754704730f219
Jongsoo Lee <leejongsoo@gmail.com>
2015-08-22 16:09:53 +0900

    CODEFORCES: Round-pi Div-2 b (complete)

--- codeforces/round-pi/div-2-b.py
+++ codeforces/round-pi/div-2-b.py
@@ -1,78 +1,25 @@
 from collections import defaultdict
-from array import array
-from itertools import repeat
-
-def bit_add(bit, idx, val):
-    """
-    >>> bit_add([0, 0, 0, 0, 0, 0, 0, 0], 2, 1)
-    [0, 0, 1, 1, 0, 0, 0, 1]
-    """
-    idx += 1
-    while idx <= len(bit):
-        bit[idx-1] += val
-        idx += (idx & -idx)
-    return bit
-
-
-def bit_sum(bit, idx):
-    """
-    >>> bit_sum([0, 1, 1, 2, 0, 0, 0, 2], 3)
-    2
-    """
-    s = 0
-    idx += 1
-    while 0 < idx:
-        s += bit[idx-1]
-        idx -= (idx & -idx)
-    return s
-
-
-def get_start(n, k):
-    """
-    >>> get_start(10, 3)
-    (10, 1)
-    >>> get_start(63, 3)
-    (7, 3)
-    >>> get_start(-6, 3)
-    (-2, 2)
-    """
-    if k == 1:
-        return n, 1
-    c= 1
-    while n % k == 0:
-        n //= k
-        c+= 1
-    return n, c
+import functools
 
 
 def main():
     N, K = tuple(int(n) for n in input().split())
     NS = tuple(int(n) for n in input().split())
-    seqs = defaultdict(lambda: [])
-    for n in NS:
-        s, c = get_start(n, K)
-        if s < 0:
-            seqs[s].append(31 - c)
-        else:
-            seqs[s].append(c)
-    ans = 0
-    for seq in seqs.values():
-        if K == 1:
-            l = len(seq)
-            if 3 <= l:
-                ans += (l*(l-1)*(l-2))//6
-        else:
-            km = [[0]*32, [0]*32, [0]*32]
-            for s in seq:
-                for k in (0, 1, 2):
-                    val = 1 if k == 0 else bit_sum(km[k-1],s-1)
-                    km[k] = bit_add(km[k], s, val)
-            ans += km[2][31]
+    nst = defaultdict(lambda: [0, 0, 0])
 
-    print(ans)
+    for n in NS:
+        if n == 0:
+            nst[0][2] += nst[0][1]
+            nst[0][1] += nst[0][0]
+            nst[0][0] += 1
+        else:
+            d, m = divmod(n, K)
+            if d != 0 and m == 0:
+                nst[n][2] += nst[n//K][1]
+                nst[n][1] += nst[n//K][0]
+            nst[n][0] += 1
+    print(functools.reduce(lambda x, y: x + y[2], nst.values(), 0))
 
 
 if __name__ == '__main__':
-    #import doctest
-    #doctest.testmod()
     main()
