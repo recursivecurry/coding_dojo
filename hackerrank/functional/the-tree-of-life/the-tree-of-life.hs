8b37dbb16c077c4df207dca21679b93db7d91905
Jongsoo Lee <leejongsoo@gmail.com>
2015-11-02 01:25:42 +0900

    HACKERRANK: The Tree Of Life
    https://www.hackerrank.com/challenges/the-tree-of-life

--- hackerrank/functional/the-tree-of-life/the-tree-of-life.hs
+++ hackerrank/functional/the-tree-of-life/the-tree-of-life.hs
@@ -1,12 +1,13 @@
+-- HACKERRANK: The Tree Of Life
+-- https://www.hackerrank.com/challenges/the-tree-of-life
 module Main where
 
-import qualified Text.Parsec as P
-import qualified Text.Parsec.String as S
-import qualified Text.Parsec.Char as CH
-import qualified Text.Parsec.Combinator as CB
+import Text.Read (readPrec, lift)
+import Text.ParserCombinators.ReadP (ReadP, char)
+import Control.Applicative
 import qualified Control.Monad as M
 import qualified Data.Array as A
-import Debug.Trace
+import Data.Bits
 
 type Status = Char
 type Path = String
@@ -14,103 +15,74 @@ type Step = Int
 
 data TreeNode = Branch TreeNode Status TreeNode | Leaf Status deriving Show
 
-type Rules = A.Array Int Status
 type Trees = A.Array Int TreeNode
 
 
 -- TREE PARSING
 
-value :: S.Parser Status
-value = do v <- (CH.char '.') P.<|> (CH.char 'X')
-           return v
+instance Read TreeNode where
+  readPrec = lift tree
 
-leaf :: S.Parser TreeNode
-leaf = do v <- value
-          return (Leaf v)
+tree :: ReadP TreeNode
+tree = Leaf <$> node
+   <|> Branch <$ char '(' <*> tree <* char ' ' <*> node <* char ' ' <*> tree <* char ')'
 
-branch :: S.Parser TreeNode
-branch = do CH.char '('
-            left <- branch P.<|> leaf
-            CH.spaces
-            root <- value
-            CH.spaces
-            right <- branch P.<|> leaf
-            CH.char ')'
-            return (Branch left root right)
+node :: ReadP Status
+node = char 'X' <|> char '.'
 
-treenode :: S.Parser TreeNode
-treenode = do t <- branch
-              P.many (CH.string "\n" P.<|> CH.string "\r" P.<|> CH.string "\r\n")
-              return  t
 
-parseTree :: String -> TreeNode
-parseTree s = case P.parse treenode "" s of
-                Left _ -> Leaf '.'
-                Right t -> t
+nv :: TreeNode -> Status
+nv (Leaf n) = n
+nv (Branch _ n _) = n
 
-nodeValue :: TreeNode -> Status
-nodeValue (Leaf n) = n
-nodeValue (Branch _ n _) = n
-
-statusToInt :: Status -> Int
-statusToInt 'X' = 1
-statusToInt _ =  0
+s2i :: Status -> Int
+s2i 'X' = 1
+s2i _ =  0
 
 
 -- RULE GENERATION
-rule :: Int -> Rules
-rule n =  A.listArray (0,15) $ reverse (toBin 16 n)
-
-ruleValue :: Rules -> Status -> TreeNode -> Status
-ruleValue r parent currentNode = case currentNode of
-                                Leaf current -> r A.! ((2^3) * (statusToInt parent) + 2 * (statusToInt current))
-                                Branch left current right -> r A.! ((2^3) * (statusToInt parent) + (2^2) * (statusToInt (nodeValue left)) + 2 * (statusToInt current) + (statusToInt (nodeValue right)))
-
-toBin :: Int -> Int -> [Status]
-toBin p n = reverse (helper n p)
-          where helper 0 r = replicate r '.'
-                helper n r | n `mod` 2 == 1 = 'X' : helper (n `div` 2) (r-1)
-                           | n `mod` 2 == 0 = '.' : helper (n `div` 2) (r-1)
+rule :: Int -> Status -> TreeNode -> Status
+rule n parent current = case current of
+                          Leaf current -> if (2 ^ ((2^3) * (s2i parent) + 2 * (s2i current)) .&. n) == 0 then '.' else 'X'
+                          Branch left current right -> if (2 ^ ((2^3) * (s2i parent) + (2^2) * (s2i (nv left)) + 2 * (s2i current) + (s2i (nv right))) .&. n) == 0 then '.' else 'X'
 
 
 -- PATH PARSING
-path :: S.Parser (Step, String)
-path = do n <- (CB.many1 CH.digit)
-          CH.spaces
-          CH.char '['
-          p <- (P.many (CH.oneOf "<>"))
-          CH.char ']'
-          return ((read n :: Int), p)
-
 
 parseCondition :: String -> (Step, String)
-parseCondition s = case P.parse path "" s of
-                Left _ -> (-1, "")
-                Right v -> v
+parseCondition s = ((read stepStr :: Int), path)
+                   where stepStr:pathStr:_ = splitBy ' ' s
+                         path = (tail . init) pathStr
+                         splitBy delimiter = foldr f [[]]
+                                             where f c l@(x:xs) | c == delimiter = []:l
+                                                                | otherwise = (c:x):xs
 
+
+-- GET VALUE OF PATH
 pathValue :: TreeNode -> Path -> Status
-pathValue tree [] = nodeValue tree
+pathValue tree [] = nv tree
 pathValue (Branch left current right) pps@(p:ps) = case p of
                                                      '<' -> pathValue left ps
                                                      '>' -> pathValue right ps
 pathValue _ _ = undefined
 
--- EVALUATE
-next :: Rules -> Status -> TreeNode -> TreeNode
-next r parent currentNode = case currentNode of
-                              Branch left value right -> Branch (next r value left) (ruleValue r parent currentNode) (next r value right)
-                              Leaf value -> Leaf (ruleValue r parent currentNode)
 
-trees :: Rules -> TreeNode -> Trees
+-- EVALUATE
+next :: (Status -> TreeNode -> Status) -> Status -> TreeNode -> TreeNode
+next r parent currentNode = case currentNode of
+                              Branch left value right -> Branch (next r value left) (r parent currentNode) (next r value right)
+                              Leaf value -> Leaf (r parent currentNode)
+
+trees :: (Status -> TreeNode -> Status) -> TreeNode -> Trees
 trees rules initialTree = A.listArray (0, 1000 * 1100) treeList
                           where treeList = initialTree : map (next rules '.') treeList
 
 main :: IO ()
 main = do ruleStr <- getLine
-          let selectedRules = rule (read ruleStr :: Int)
+          let selectedRule = rule (read ruleStr :: Int)
           treeStr <- getLine
-          let initialTree = parseTree treeStr
-          let selectedTrees = trees selectedRules initialTree
+          let initialTree = read treeStr :: TreeNode
+          let selectedTrees = trees selectedRule initialTree
           caseStr <- getLine
           let caseNum = read caseStr :: Int
           M.foldM_ (\prevStep i -> do conditionStr <- getLine
