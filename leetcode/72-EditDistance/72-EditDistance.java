e4d58d46cb00b26f5dcd10bd12c14e3e98696afe
Jongsoo Lee <leejongsoo@gmail.com>
2016-11-10 22:36:38 +0900

    LeetCode: 72. Edit Distance

---
+++ leetcode/72-EditDistance/72-EditDistance.java
@@ -0,0 +1,37 @@
+public class Solution {
+
+    public int minDistance(String word1, String word2) {
+        int word1Length = word1.length();
+        int word2Length = word2.length();
+
+        int[][] cache = new int[word1Length+1][word2Length+1];
+
+        for (int i = 0; i <= word1Length; i++) {
+            cache[i][0] = i;
+        }
+
+        for (int j = 0; j <= word2Length; j++) {
+            cache[0][j] = j;
+        }
+
+        for (int i = 0; i < word1Length; i++) {
+            char ch1 = word1.charAt(i);
+            for (int j = 0; j < word2Length; j++) {
+                char ch2 = word2.charAt(j);
+
+                if (ch1 == ch2) {
+                    cache[i+1][j+1] = cache[i][j];
+                } else {
+                    int insert = cache[i][j+1] + 1;
+                    int replace = cache[i][j] + 1;
+                    int delete = cache[i+1][j] + 1;
+
+                    cache[i+1][j+1] = (insert > delete) ? (delete < replace ? delete : replace) : (insert < replace ? insert : replace);
+                }
+            }
+        }
+        return cache[word1Length][word2Length];
+    }
+}
+
+
