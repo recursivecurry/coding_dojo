0d9c6bed62da296f7a64bcac966c8cde3e3c025a
Jongsoo Lee <leejongsoo@gmail.com>
2016-11-17 23:25:34 +0900

    LeetCode: 111 Minimum Depth Of Binary Tree

---
+++ leetcode/111-MinimumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Solution {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public int minDepth(TreeNode root) {
+        Set<TreeNode> current = new HashSet<>();
+        if (root != null) {
+            current.add(root);
+        }
+        int depth = 0;
+
+        while (!current.isEmpty()) {
+            depth += 1;
+            Set<TreeNode> next = new HashSet<>();
+            for (TreeNode node : current) {
+                if (node != null && node.left != null) {
+                    next.add(node.left);
+                }
+                if (node != null && node.right != null) {
+                    next.add(node.right);
+                }
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+            }
+            current = next;
+        }
+        return depth;
+    }
+}
