b6802a68a5d01b067d203beb3ff6877db5036177
Jongsoo Lee <leejongsoo@gmail.com>
2016-11-24 01:29:26 +0900

    update template

---
+++ template/java/src/main/java/Solution.java
@@ -0,0 +1,9 @@
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
+
---
+++ template/java/src/test/java/SolutionTest.java
@@ -0,0 +1,12 @@
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void test() throws Exception {
+        Solution solution = new Solution();
+    }
+
+}
