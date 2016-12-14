3d8ab606f1d267d8dd31536aa4fc1049a3835043
Jongsoo Lee <leejongsoo@gmail.com>
2016-12-14 23:28:20 +0900

    LeetCode: 406. Queue Reconstruction by Height

--- leetcode/406-QueueReconstructionByHeight/Solution.java
+++ leetcode/406-QueueReconstructionByHeight/Solution.java
@@ -1,16 +1,12 @@
 public class Solution {
     public int[][] reconstructQueue(int[][] people) {
         int[][] result = new int[people.length][2];
-        int last = 0;
 
-        Arrays.sort(people, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) {
-                    return (o1[1]-o2[1]);
-                } else {
-                    return -(o1[0] - o2[0]);
-                }
+        Arrays.sort(people, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1]-o2[1];
+            } else {
+                return -(o1[0] - o2[0]);
             }
         });
 
@@ -18,29 +14,13 @@ public class Solution {
             insert(result, p);
         }
 
-        return people;
-    }
-
-    private static boolean isEmpty(int[] p) {
-        return p[0]==0 && p[1]==0;
+        return result;
     }
 
     private static void insert(int[][] result, int[] p) {
-        for (int i=0; i<result.length; i++) {
-            if (isEmpty(result[i])) {
-                result[i] = p;
-            }
-            if (i==p[i]) {
-                
-            }
+        for (int i=result.length-1; i>p[1]; i--) {
+            result[i] = result[i-1];
         }
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[][] result = solution.reconstructQueue(new int[][]{new int[]{7,0}, new int[]{4,4}, new int[]{7,1}, new int[]{5,0}, new int[]{6,1}, new int[]{5,2}});
-        for (int[] i : result) {
-            log.info("{},{}", i[0], i[1]);
-        }
+        result[p[1]] = p;
     }
 }
