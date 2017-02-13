8d2f5e16ffc887454fecd938661bace107a218e6
Jongsoo Lee <leejongsoo@gmail.com>
2017-02-13 23:52:10 +0900

    add cline c++ template

--- template/cpp/main.cpp
+++ template/cpp/main.cpp
@@ -1,59 +1,28 @@
-/*
- * HEADER
- */
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-
-#include <iostream>
-
-#include <array>
-#include <deque>
-#include <forward_list>
-#include <list>
 #include <map>
-#include <queue>
 #include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
 #include <stack>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-
-#include <algorithm>
-#include <bitset>
-#include <functional>
-#include <limits>
-#include <numeric>
-#include <regex>
 #include <string>
-#include <utility>
-
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
-/*
- * PRE-DEFINED FUNCTION
- */
-
-// Power function for Integer
-long long POWI(long long base, long long exp) {
-    long long result = 1;
-
-    while (exp) {
-        if (exp & 1)
-            result *= base;
-        exp >>= 1;
-        base *= base;
-    }
-
-    return result;
+int main() {
+    return 0;
 }
-
-// Factorial
-
-
-int main (int argc, char *argv[]) {
-    cout << POWI(2,0) << ", " << POWI(2,1) << ", " << POWI(2, 10) << endl;
-    cout << POWI(3,0) << ", " << POWI(3,1) << ", " << POWI(3, 10) << endl;
-}
-
