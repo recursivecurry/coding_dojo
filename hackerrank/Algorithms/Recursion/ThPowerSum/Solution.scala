ccee51d25eac18a212363c1910c2428c904ee0d3
Jongsoo Lee <everyevery@users.noreply.github.com>
2017-04-05 00:52:59 +0900

    HackerRank: The Power Sum

---
+++ hackerrank/Algorithms/Recursion/ThPowerSum/Solution.scala
@@ -0,0 +1,15 @@
+object Solution {
+  def numberOfWays(X:Int,N:Int,C:Int):Int = {
+    if (X == 0)
+      1
+    else
+      Stream.from(1)
+        .takeWhile(n => math.pow(n, N) <= X)
+        .map(n => numberOfWays(X - math.pow(n, N).toInt, N, n + 1))
+        .sum
+  }
+
+  def main(args: Array[String]) {
+    println(numberOfWays(readInt(),readInt(), 1))
+  }
+}
