436ec71724971ab1e1eb67e9643565b34b9d8ca9
Jongsoo Lee <453247+recursivecurry@users.noreply.github.com>
2023-07-11 22:29:39 +0900

    Update main.rs

--- leetcode/605-CanPlaceFlowers/main.rs
+++ leetcode/605-CanPlaceFlowers/main.rs
@@ -1,14 +1,12 @@
-struct Solution;
-
 impl Solution {
     pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
-        let mut zero = 0;
+        let mut zero = 1;
         let mut count = 0;
         for p in flowerbed {
             if p == 0 {
                 zero += 1;
             } else {
-                count += zero / 2;
+                count += if zero > 1 { (zero-1) / 2} else { 0 };
                 zero = 0;
                 if count >= n {
                     break
@@ -31,6 +29,10 @@ mod test {
         );
         assert_eq!(
             Solution::can_place_flowers(vec![1, 0, 0, 0, 1], 2),
+            false
+        );
+        assert_eq!(
+            Solution::can_place_flowers(vec![1, 0, 0, 0, 0, 1], 2),
             false
         );
     }
